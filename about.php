e4c1b0a71dda83e607a3534266b6b4cf8626a298
buche2 <buchs.enrico@gmail.com>
2016-09-26 13:56:24 +0200

    added about page

---
+++ about.php
@@ -0,0 +1,13 @@
+<?php
+$project = "BIS15-Bootstrap";
+$jumbo = array('title'=>'Etwas anderes','lead'=>'Lead Text blablabla');
+
+$blocks = array(array('title'=>'first','danger'=>'Gefährlich','lead'=>'leadtext',
+'text'=>'der text'),
+array('title'=>'second','lead'=>'leadtext 2',
+'text'=>'der text'),
+array('title'=>'third','lead'=>'leadtext 3',
+'text'=>'der text'));
+
+
+include 'template/bootstrap-justified.php';
