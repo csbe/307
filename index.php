3ca7a094758f56253a1949ba21c9ad7bedd25d9d
buche2 <buchs.enrico@gmail.com>
2016-09-26 13:36:35 +0200

    bootstrap, jquery, templating example

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+$project = "BIS15-Bootstrap";
+$jumbo = array('title'=>'Dynamisch','lead'=>'Lead Text dynamisch');
+
+$blocks = array(array('title'=>'first','danger'=>'Gefährlich','lead'=>'leadtext',
+'text'=>'der text'),
+array('title'=>'second','lead'=>'leadtext 2',
+'text'=>'der text'),
+array('title'=>'third','lead'=>'leadtext 3',
+'text'=>'der text'));
+
+
+$js = '$(document).ready(function(){
+	setTimeout(function(){	
+	$.ajax({
+		url: "form.php",	
+		success: function(result){
+			$(".jumbotron").html(result);
+		},
+	});
+},10000);
+});';
+
+include 'template/bootstrap-justified.php';
---
+++ template/bootstrap-justified.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="Enrico Buchs" >
+    <link rel="icon" href="favicon.ico">
+
+    <title>Justified Nav Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.0/themes/smoothness/jquery-ui.css">
+
+    <!-- Custom styles for this template -->
+    <link href="css/justified-nav.css" rel="stylesheet">
+	
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>	
+		<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.0/jquery-ui.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>    
+    
+    
+    <?php if(isset($js) && $js != null){ ?>  	
+    <script type="text/javascript" >
+    	<?php echo $js; ?>
+    </script>
+    <?php } ?>
+
+  </head>
+
+  <body>
+
+    <div class="container">
+
+      <!-- The justified navigation menu is meant for single line per list item.
+           Multiple lines will require custom code not provided by Bootstrap. -->
+      <div class="masthead">
+        <h3 class="text-muted"><?php echo $project ?></h3>
+        <nav>
+          <ul class="nav nav-justified">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#">Projects</a></li>
+            <li><a href="http://google.ch">Services</a></li>
+            <li><a href="#">Downloads</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+        </nav>
+      </div>
+
+      <!-- Jumbotron -->
+      <div class="jumbotron">
+        <h1><?php echo $jumbo['title']; ?></h1>
+        <p class="lead"><?php echo $jumbo['lead']; ?></p>
+        <p><a class="btn btn-lg btn-success" href="#" role="button">Get started today</a></p>       
+      </div>
+
+      <!-- Example row of columns -->
+      <div class="row">
+      	<?php foreach($blocks as $block){
+      		?>
+      			<div class="col-lg-4">
+			          <h2><?php echo $block['title']; ?></h2>
+			          <?php if(isset($block['danger'])) { ?>
+			          <p class="text-danger"><?php echo $block['danger']; ?></p>
+			          <?php } ?>
+			          <p><?php echo $block['lead']; ?></p>
+			          <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
+			        </div>
+      		
+      		<?php
+      	}
+      	?>
+      	<!--
+        <div class="col-lg-4">
+          <h2>Safari bug warning!</h2>
+          <p class="text-danger">As of v9.1.2, Safari exhibits a bug in which resizing your browser horizontally causes rendering errors in the justified nav that are cleared upon refreshing.</p>
+          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+          <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
+        </div>
+        <div class="col-lg-4">
+          <h2>Heading</h2>
+          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+          <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
+       </div>
+        <div class="col-lg-4">
+          <h2>Heading</h2>
+          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa.</p>
+          <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
+        </div>-->
+      </div>
+
+      <!-- Site footer -->
+      <footer class="footer">
+        <p>&copy; 2016 Company, Inc.</p>
+      </footer>
+
+    </div> <!-- /container -->
+    
+
+  </body>
+</html>
