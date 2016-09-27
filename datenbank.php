aa478db820fd9a7d26519f1d029215ad149d911c
buche2 <buchs.enrico@gmail.com>
2016-09-27 10:44:52 +0200

    database example with sqlite3

---
+++ datenbank.php
@@ -0,0 +1,23 @@
+<?php
+phpinfo();
+//http://localhost/bis15/307/datenbank.php?page=2 union select '' as jumbotitle, '' as jumbolead,username as title,password as lead, '' from user;
+
+$project = "BIS15-Datenbank Beispiel";
+
+$sqlite = new SQLite3('db/307.db');
+
+$page = (isset($_GET['page']))? $_GET['page']:1;
+// $page = 1 or page.id = 2
+/*$statement = $sqlite->prepare("select jumbo as jumbotitle,page.lead as jumbolead, content.title, content.lead, content.content from page left join content on content.pagefk = page.id where page.id = :id;");
+$statement->bindValue(':id',$page);
+$result = $statement->execute();*/
+$result = $sqlite->query("select jumbo as jumbotitle,page.lead as jumbolead, content.title, content.lead, content.content from page left join content on content.pagefk = page.id where page.id = $page;");
+
+$row = $result->fetchArray();
+$jumbo = array('title'=>$row['jumbotitle'],'lead'=>$row['jumbolead']);
+$blocks = array();
+do{
+ $blocks[] = $row;
+}while($row = $result->fetchArray());
+
+include 'template/bootstrap-justified.php';
--- form.php
+++ form.php
@@ -1,32 +1,51 @@
-    <script type="text/javascript" >
-    	$("#date").datepicker({
-			dateFormat: 'dd.mm.yy',
- 			minDate: "-60Y",
-			maxDate: "+10D",
-			showWeek: true,
-			changeYear: true,
-			changeMonth:false,
-			    	
-    	});
-    </script>
-<form method="post" action="" enctype="multipart/form-data">
+<script type="text/javascript" >
+	// datepicker example
+ 	$("#date").datepicker({
+		dateFormat: 'dd.mm.yy',
+	 			minDate: "-60Y",
+		maxDate: "+10D",
+		showWeek: true,
+		changeYear: true,
+		changeMonth:false,	    	
+ 	});
+ 	
+ 	// send form with ajax
+ 	$("#myform").on('submit', function () {
+ 		form = $(this);
+ 		$.post(form.attr("action"), form.serialize())
+ 						.done(function (data) {
+				 			form.parent().html(data);	
+				 			$(".message").dialog();
+				 		});
+ 		
+ 		return false;
+ 	});
+</script>
+<?php
+	if(isset($_POST['date'])) {
+		echo "<div class='message'>Speichern erfolgreich</div>";
+	}
+?>
+
+
+<form id="myform" method="post" action="form.php" enctype="multipart/form-data">
   <div class="form-group">
     <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email" required name="email">
   </div>
   <div class="form-group">
     <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" name="password">
   </div>
   <div class="form-group">
     <label for="exampleInputFile">File input</label>
-    <input type="file" id="exampleInputFile">
+    <input type="file" id="exampleInputFile" name="file">
     <p class="help-block">Example block-level help text here.</p>
   </div>
    <div class="form-group">
    
     <label for="date">Date</label>
-    <input type="date" id="date">
+    <input type="date" id="date" required name="date">
     
   </div>
   <div class="checkbox">
